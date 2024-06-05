4db098b01d886917a3877f195d0e87fb6f227816
Khun <khun.ly@bstorm.be>
2024-06-05 16:48:16 +0200

    test

--- Connect4.API/Hubs/GameHub.cs
+++ Connect4.API/Hubs/GameHub.cs
@@ -7,27 +7,27 @@ namespace Connect4.API.Hubs
 {
     public class GameHub(TableService tableService) : Hub
     {
-        public void Create(string name)
+        public async void Create(string name)
         {
             TableBO table = tableService.CreateTable(name);
-            Groups.AddToGroupAsync(Context.ConnectionId, table.Name);
-            Clients.All.SendAsync("AllTables", tableService.Tables.Select(t => new TableDTO(t)));
-            Clients.Group(table.Name).SendAsync("CurrentGame", new TableDetailledDTO(table));
+            await Groups.AddToGroupAsync(Context.ConnectionId, table.Name);
+            await Clients.All.SendAsync("AllTables", tableService.Tables.Select(t => new TableDTO(t)));
+            await Clients.Group(table.Name).SendAsync("CurrentGame", new TableDetailledDTO(table));
         }
 
-        public void Join(JoinTableDTO dto)
+        public async void Join(JoinTableDTO dto)
         {
             TableBO table = tableService.Join(dto.TableName, dto.PlayerName);
-            Groups.AddToGroupAsync(Context.ConnectionId, table.Name);
-            Clients.All.SendAsync("AllTables", tableService.Tables.Select(t => new TableDTO(t)));
-            Clients.Group(table.Name).SendAsync("CurrentGame", new TableDetailledDTO(table));
+            await Groups.AddToGroupAsync(Context.ConnectionId, table.Name);
+            await Clients.All.SendAsync("AllTables", tableService.Tables.Select(t => new TableDTO(t)));
+            await Clients.Group(table.Name).SendAsync("CurrentGame", new TableDetailledDTO(table));
         }
 
-        public void Play(PlayDTO dto)
+        public async void Play(PlayDTO dto)
         {
             TableBO table = tableService.GetByName(dto.TableName);
             table.Grid.Play(dto.Col);
-            Clients.Group(table.Name).SendAsync("CurrentGame", new TableDetailledDTO(table));
+            await Clients.Group(table.Name).SendAsync("CurrentGame", new TableDetailledDTO(table));
         }
 
         public override async Task OnConnectedAsync()
