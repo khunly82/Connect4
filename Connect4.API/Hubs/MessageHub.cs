f6408c0b780b885b91fc156aeda29cb668434902
Khun <khun.ly@bstorm.be>
2024-06-05 09:34:02 +0200

    Add project files.

---
+++ Connect4.API/Hubs/MessageHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Connect4.API.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            // envoyer à la personne qui vient de se connecter
+            Clients.Caller.SendAsync("info", "Vous êtes bien connecté");
+
+            // envoyer à tous les autres
+            Clients.Others.SendAsync("info", "Un autre utilisateur  s'est connecté");
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Connect4.API/Program.cs
@@ -0,0 +1,37 @@
+using Connect4.API.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddCors(b => b.AddDefaultPolicy(o =>
+{
+    o.WithOrigins("http://localhost:4200");
+    o.AllowCredentials();
+    o.AllowAnyHeader();
+    o.AllowAnyMethod();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<MessageHub>("ws/message");
+
+app.Run();
